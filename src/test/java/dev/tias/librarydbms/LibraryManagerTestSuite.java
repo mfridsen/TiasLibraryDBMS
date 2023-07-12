95da325260140465dae70a35cc9eaa0831bd0840
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-07-12 21:30:01 +0200

    Trying to implement logging, no luck so far.

--- src/test/java/dev/tias/librarydbms/LibraryManagerTestSuite.java
+++ src/test/java/dev/tias/librarydbms/LibraryManagerTestSuite.java
@@ -3,9 +3,13 @@ package dev.tias.librarydbms;
 import dev.tias.librarydbms.control.ControlTestSuite;
 import dev.tias.librarydbms.model.ModelTestSuite;
 import dev.tias.librarydbms.service.ServiceTestSuite;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Mattias Fridsén
@@ -19,7 +23,7 @@ import org.junit.platform.suite.api.Suite;
  * <p>
  * Brought to you by inhumane amounts of tobacco.
  */
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Suite
 @SelectClasses({
         ModelTestSuite.class,
@@ -28,9 +32,31 @@ import org.junit.platform.suite.api.Suite;
         //View tests excluded cause I have no idea how to test GUIs yet lol
         //ViewTestSuite.class
 })
-
-public class
-LibraryManagerTestSuite
+public class LibraryManagerTestSuite
 {
+    private static final Logger logger;
+    private static long startTime;
 
+    static
+    {
+        System.setProperty("logFileName", "logs/tests/LibraryManagerTestSuite");
+        logger = LoggerFactory.getLogger(LibraryManagerTestSuite.class);
+    }
+
+    @BeforeAll
+    public void setUp()
+    {
+        System.out.println("SETTING UP TEST SUITE");
+        logger.info("starting");
+        startTime = System.currentTimeMillis();
+    }
+
+    @AfterAll
+    public void tearDown()
+    {
+        System.out.println("TEARING DOWN TEST SUITE");
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        logger.info("Test suite took: " + duration + "ms");
+    }
 }
