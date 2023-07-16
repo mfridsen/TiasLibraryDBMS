ffc7fc74cf1e511b87abaa4f55dc8d830036f922
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-07-16 22:46:55 +0200

    Updated test method prints.

---
+++ src/test/java/dev/tias/librarydbms/model/BaseModelTest.java
@@ -0,0 +1,23 @@
+package dev.tias.librarydbms.model;/**
+  * @project TiasLibraryDBMS
+  * @package dev.tias.librarydbms.model
+  * @author Mattias Fridsén
+  * @contact matfir-1@student.ltu.se
+  * @date 7/16/2023
+  *
+  * We plan as much as we can (based on the knowledge available), 
+  * When we can (based on the time and resources available),
+  * But not before.
+  *
+  * Brought to you by enough nicotine to kill a large horse.
+  */
+ public class BaseModelTest {
+    
+    public BaseModelTest() {
+    
+    }
+
+    /*********************************** Getters and Setters are self-explanatory. ************************************/
+    
+   
+}
